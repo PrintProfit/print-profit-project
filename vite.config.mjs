d0eb921376c1640b639e802df6cbacdfa8bc1537
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-28 23:29:24 -0600

    Simplified vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,16 +1,14 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: 'build',
+export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5001',
     },
-    server: {
-      proxy: {
-        '/api': 'http://localhost:5001',
-      },
-    },
-    plugins: [react()],
-  };
+  },
+  plugins: [react()],
 });
