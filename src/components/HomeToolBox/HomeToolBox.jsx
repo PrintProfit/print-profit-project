037d4450465e828f55cfe63b3c4f10b8148f102c
Marcos Andrade <mnandrade1999@gmail.com>
2024-01-29 11:41:07 -0600

    added routing for sidebar, doesn't work yet, home page is laid out with skeleton

--- src/components/HomeToolBox/HomeToolBox.jsx
+++ src/components/HomeToolBox/HomeToolBox.jsx
@@ -5,7 +5,7 @@ export default function HomeToolBox() {
   return (
     <>
       <Box>
-        <Box>
+        <Box border={3}>
           <h2>Nested Box</h2>
         </Box>
         <h2>Tutorial</h2>
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -10,11 +10,17 @@ import ListItemText from '@mui/material/ListItemText';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import UserPage from '../UserPage/UserPage';
 
 const drawerWidth = 240;
 
 export default function SideBar() {
+  const HandleSideBarNav = (e) => {
+    console.log('SideBar Nav Clicked');
+    console.log('e.target:', e.target);
+  };
+
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
@@ -43,16 +49,15 @@ export default function SideBar() {
         <Toolbar />
         <Divider />
         <List>
-          {['Tools Header', 'C&P', 'Tool2', 'About/Contact'].map(
-            (text, index) => (
-              // Tools Header does not need to be a button, should make bold and add divider
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ),
-          )}
+          <h3>Tools Header</h3>
+          {['C&P', 'Tool2', 'About/Contact'].map((text, index) => (
+            // Tools Header does not need to be a button, should make bold and add divider
+            <ListItem key={text} disablePadding>
+              <ListItemButton component={Link} to="/about">
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
         </List>
         <Divider />
       </Drawer>
