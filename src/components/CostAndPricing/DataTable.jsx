03d9b82ad21eb6c62e349f01b7b2e0b25fa32931
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-30 15:37:45 -0600

    Made stuff in the data table use grids instead of tables

--- src/components/CostAndPricing/DataTable.jsx
+++ src/components/CostAndPricing/DataTable.jsx
@@ -1,11 +1,5 @@
 // @ts-check
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-} from '@mui/material';
+import { TableCell, Unstable_Grid2 as Grid } from '@mui/material';
 import {
   flexRender,
   getCoreRowModel,
@@ -27,21 +21,21 @@ export function DataTable({ data, columns }) {
   // This will be arranged the way it's supposed to later, but for now,
   // I can make this faster with normal tables.
   return (
-    <Table>
-      <TableHead>
+    <Grid container>
+      <Grid container>
         {/*
         This is how you get the header groups in tanstack tables. It's needed
         to get the header groups in general, but it's pretty likely that the
         table won't actually support having multiple header groups.
         */}
         {table.getHeaderGroups().map((group) => (
-          <TableRow key={group.id}>
+          <Grid container direction="column" key={group.id}>
             {/*
             This is how you get the headers within a group for tanstack tables.
             We can just map them into table cells.
             */}
             {group.headers.map((header) => (
-              <TableCell key={header.id}>
+              <TableCell variant="head" key={header.id}>
                 {header.isPlaceholder ||
                   flexRender(
                     header.column.columnDef.header,
@@ -49,15 +43,15 @@ export function DataTable({ data, columns }) {
                   )}
               </TableCell>
             ))}
-          </TableRow>
+          </Grid>
         ))}
-      </TableHead>
-      <TableBody>
+      </Grid>
+      <Grid container>
         {/*
         This is how we get each row in tanstack tables.
         */}
         {table.getRowModel().rows.map((row) => (
-          <TableRow key={row.id}>
+          <Grid container direction="column" key={row.id}>
             {/*
             This is how we get the cells via tanstack tables
             */}
@@ -70,9 +64,9 @@ export function DataTable({ data, columns }) {
                 {flexRender(cell.column.columnDef.cell, cell.getContext())}
               </TableCell>
             ))}
-          </TableRow>
+          </Grid>
         ))}
-      </TableBody>
-    </Table>
+      </Grid>
+    </Grid>
   );
 }
