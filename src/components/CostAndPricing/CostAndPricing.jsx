c6ae495849e48e8b9555a7339e2e33cc87a99394
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-30 15:29:22 -0600

    Added a bit of padding to the cost & pricing tool to get stuff below the app bar

--- src/components/CostAndPricing/CostAndPricing.jsx
+++ src/components/CostAndPricing/CostAndPricing.jsx
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material';
 import { useState } from 'react';
 import { PricingTable } from './PricingTable';
 import { quote as sampleQuote } from './sample-data';
@@ -5,9 +6,14 @@ import { quote as sampleQuote } from './sample-data';
 export default function CostAndPricing() {
   const [quote, setQuote] = useState(sampleQuote);
 
+  // The sx prop should get removed later when the overall app layout is finished
   return (
-    <div>
+    <Box
+      sx={{
+        padding: '3rem',
+      }}
+    >
       <PricingTable quote={quote} setQuote={setQuote} />
-    </div>
+    </Box>
   );
 }
