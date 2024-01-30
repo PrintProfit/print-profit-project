dad475807b1181320b6031b8e6033b9439de7430
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-30 10:17:19 -0600

    Wrapped quote in `useState`

--- src/components/CostAndPricing/CostAndPricing.jsx
+++ src/components/CostAndPricing/CostAndPricing.jsx
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import { PricingTable } from './PricingTable';
 import { quote as sampleQuote } from './sample-data';
 
 export default function CostAndPricing() {
+  const [quote, setQuote] = useState(sampleQuote);
+
   return (
     <div>
-      <PricingTable quote={sampleQuote} />
+      <PricingTable quote={quote} />
     </div>
   );
 }
