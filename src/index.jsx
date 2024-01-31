c181058b535d72fe5bc13f1443aeb6594d99d10f
Marcos Andrade <mnandrade1999@gmail.com>
2024-01-31 15:02:38 -0600

    added Cssbaseline to root of project index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -16,6 +16,7 @@ const root = ReactDOM.createRoot(document.getElementById('react-root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Provider store={store}>
         <App />
       </Provider>
