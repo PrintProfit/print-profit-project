2d0e2512949a328c78eb45de77c21e87915001a4
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-30 18:31:04 -0600

    Added an empty history tab component and some comments

--- src/components/CostAndPricing/CostAndPricing.jsx
+++ src/components/CostAndPricing/CostAndPricing.jsx
@@ -1,6 +1,12 @@
+// @ts-check
 import { Box } from '@mui/material';
 import { PricingTab } from './PricingTab';
 
+/**
+ * Later on, this could be tweaked to use links for tabs, but right now that's
+ * not being done.
+ * @see {@link https://mui.com/material-ui/guides/routing/#tabs Routing Libraries - Material UI}
+ */
 export default function CostAndPricing() {
   return (
     <Box sx={{ padding: '3rem' }}>
