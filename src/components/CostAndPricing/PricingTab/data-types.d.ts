54bd1f9bc15b82f0549dc263b14cc9b148b27099
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-02-05 14:19:59 -0600

    Added IDs to quote types

--- src/components/CostAndPricing/PricingTab/data-types.d.ts
+++ src/components/CostAndPricing/PricingTab/data-types.d.ts
@@ -3,11 +3,13 @@
 import type { ColumnDef } from '@tanstack/react-table';
 
 export interface Cost {
+  id?: number;
   name: string;
   value: number;
 }
 
 export interface Product {
+  id?: number;
   name: string;
   quantity: number;
   selling_price_per_unit: number;
@@ -17,6 +19,7 @@ export interface Product {
 }
 
 export interface Quote {
+  id?: number;
   name: string;
   manual_contribution_percent?: number;
   manual_total_selling_price?: number;
