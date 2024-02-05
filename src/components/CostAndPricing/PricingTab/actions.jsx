64317df7e53c085780a64ccbe3c3b4a2fdf5c0ff
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-02-05 10:18:03 -0600

    Added dialog to save quote

--- src/components/CostAndPricing/PricingTab/actions.jsx
+++ src/components/CostAndPricing/PricingTab/actions.jsx
@@ -1,6 +1,16 @@
 // @ts-check
 
-import { Button, Stack } from '@mui/material';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Stack,
+  TextField,
+} from '@mui/material';
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 /**
@@ -22,13 +32,62 @@ export function QuoteActions({ quote }) {
 function SaveQuote({ quote }) {
   const dispatch = useDispatch();
 
-  const handleSave = () => {
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState(quote.name ?? '');
+
+  const saveQuote = () => {
     dispatch({ type: 'SAGA/SAVE_QUOTE', payload: quote });
   };
 
+  /**
+   * @param {import('react').FormEvent<HTMLFormElement>} e
+   */
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    saveQuote();
+    setOpen(false);
+  };
+
+  const closeDialog = () => {
+    setOpen(false);
+    setName(quote.name ?? '');
+  };
+
   return (
-    <Button type="button" onClick={handleSave}>
-      Save
-    </Button>
+    <>
+      <Button type="button" onClick={() => setOpen(true)}>
+        Save
+      </Button>
+      <Dialog
+        open={open}
+        onClose={() => setOpen(false)}
+        PaperProps={{
+          component: 'form',
+          onSubmit: handleSubmit,
+        }}
+      >
+        <DialogTitle>Save Quote</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please specify a name for the quote.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            fullWidth
+            id="quoteName"
+            name="quoteName"
+            label="Quote Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeDialog}>Cancel</Button>
+          <Button type="submit">Save</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
