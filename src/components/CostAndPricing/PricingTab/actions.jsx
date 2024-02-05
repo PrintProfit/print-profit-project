1395ca2de338cdf6629f7cc62d3610670f0bb296
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-02-05 09:58:10 -0600

    Added handler to save quote button

--- src/components/CostAndPricing/PricingTab/actions.jsx
+++ src/components/CostAndPricing/PricingTab/actions.jsx
@@ -1,16 +1,25 @@
 // @ts-check
 
 import { Button, Stack } from '@mui/material';
+import { useDispatch } from 'react-redux';
 
 /**
  *
  * @param {import("./prop-types").QuoteActionsProps} props
  */
 export function QuoteActions({ quote }) {
+  const dispatch = useDispatch();
+
+  const handleSave = () => {
+    dispatch({ type: 'SAGA/SAVE_QUOTE', payload: quote });
+  };
+
   return (
     <Stack direction="row" spacing={2}>
-      <Button>Save</Button>
-      <Button>Update</Button>
+      <Button type="button" onClick={handleSave}>
+        Save
+      </Button>
+      <Button type="button">Update</Button>
     </Stack>
   );
 }
