bcc1cb30789e732320d6c23ed0678d111c81bcd3
Marcos Andrade <mnandrade1999@gmail.com>
2024-02-05 15:42:39 -0600

    Added template for Cards on about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,17 +1,43 @@
+import { AccountCircle, Email, LocationOn } from '@mui/icons-material';
+import { Card, CardContent, Typography } from '@mui/material';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 import React from 'react';
-
-// This is one of our simplest components
-// It doesn't have local state,
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is'
 
 function AboutPage() {
   return (
-    <div className="container">
-      <div>
-        <p>This about page is for anyone to read!</p>
-      </div>
-    </div>
+    <Grid
+      paddingTop={8}
+      container
+      spacing={3}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Card>
+        <CardContent>
+          <AccountCircle />
+          <Typography variant="h6">Contact</Typography>
+          <Typography variant="body2">Software Engineer</Typography>
+          <Button>Contact</Button>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardContent>
+          <Email />
+          <Typography variant="h6">john.doe@example.com</Typography>
+          <Typography variant="body2">Contact Email</Typography>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardContent>
+          <LocationOn />
+          <Typography variant="h6">New York, USA</Typography>
+          <Typography variant="body2">Location</Typography>
+        </CardContent>
+      </Card>
+    </Grid>
   );
 }
 
