45d49be75d79bb09889a6da241f9322142f50d50
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-02-06 14:34:54 -0600

    Added module containing some number formatters

---
+++ src/components/CostAndPricing/PricingTab/formats.js
@@ -0,0 +1,39 @@
+/**
+ * Formats a number as a percent.
+ * @param {number} [n]
+ * @returns {string | undefined}
+ */
+export function percent(n) {
+  return Number.isFinite(n)
+    ? n.toLocaleString(undefined, { style: 'percent' })
+    : undefined;
+}
+
+/**
+ * Formats a number as a currency.
+ * @param {number} [n]
+ * @returns {string | undefined}
+ */
+export function currency(n) {
+  return Number.isFinite(n)
+    ? n.toLocaleString(undefined, {
+        style: 'currency',
+        currency: 'USD',
+      })
+    : undefined;
+}
+
+/**
+ * Formats a number as a currency with accounting style.
+ * @param {number} [n]
+ * @returns {string | undefined}
+ */
+export function accounting(n) {
+  return Number.isFinite(n)
+    ? n.toLocaleString(undefined, {
+        style: 'currency',
+        currency: 'USD',
+        currencySign: 'accounting',
+      })
+    : undefined;
+}
