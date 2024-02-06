170b75065bdb3621f85460a3bbbeca5287551f27
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-02-05 18:17:32 -0600

    Added rejectUnapproved & rejectNonAdmin middleware to auth.cjs

--- server/middleware/auth.cjs
+++ server/middleware/auth.cjs
@@ -13,4 +13,22 @@ const rejectUnauthenticated = (req, res, next) => {
   }
 };
 
-module.exports = { rejectUnauthenticated };
+/** @type {import("express").RequestHandler} */
+const rejectUnapproved = (req, res, next) => {
+  if (req.isAuthenticated() && !req.user.is_removed && req.user.is_approved) {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+};
+
+/** @type {import("express").RequestHandler} */
+const rejectNonAdmin = (req, res, next) => {
+  if (req.isAuthenticated() && !req.user.is_removed && req.user.is_admin) {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+};
+
+module.exports = { rejectUnauthenticated, rejectUnapproved, rejectNonAdmin };
