ef696f3db5cf4bd158732b16df337c51a7f529ec
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-02-07 09:29:30 -0600

    Defined type for maximally damaged quote structure

--- src/components/CostAndPricing/PricingTab/data-types.d.ts
+++ src/components/CostAndPricing/PricingTab/data-types.d.ts
@@ -2,11 +2,17 @@
 
 import type { ColumnDef } from '@tanstack/react-table';
 
+type DamagedStruct<T> = {
+  [P in keyof T]?: T[P] | null;
+};
+
 export interface Cost {
   id?: number;
   name: string;
   value: number;
 }
+
+type DamagedCost = DamagedStruct<Cost>;
 
 export interface Product {
   id?: number;
@@ -18,6 +24,12 @@ export interface Product {
   costs?: Cost[];
 }
 
+type DamagedProduct = DamagedStruct<
+  Omit<Product, 'costs'> & {
+    costs?: DamagedCost[];
+  }
+>;
+
 export interface Quote {
   id?: number;
   name: string;
@@ -27,5 +39,13 @@ export interface Quote {
   created_by?: string;
   products?: Product[];
 }
+
+// This type represents the absolute maximum degree of bad data the client
+// can accept from the server. Every property can be null or undefined.
+export type DamaagedQuote = DamagedStruct<
+  Omit<Quote, 'products'> & {
+    products?: DamagedProduct[];
+  }
+>;
 
 export type ProductColumnDef = ColumnDef<Product>;
