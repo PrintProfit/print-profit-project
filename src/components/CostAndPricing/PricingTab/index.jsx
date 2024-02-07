631885c96c133e668944e47a8674a8aa8130a7b5
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-02-07 09:48:42 -0600

    Made current quote selection repair damageed quotes

--- src/components/CostAndPricing/PricingTab/index.jsx
+++ src/components/CostAndPricing/PricingTab/index.jsx
@@ -2,10 +2,11 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { PricingTable } from './PricingTable';
+import { repairQuote } from './data-repair';
 
 export function PricingTab() {
   /** @type {import('./data-types').Quote} */
-  const currentQuote = useSelector((state) => state.quote.current);
+  const currentQuote = useSelector((state) => repairQuote(state.quote.current));
   const [quote, setQuote] = useState(currentQuote);
 
   return <PricingTable quote={quote} setQuote={setQuote} />;
