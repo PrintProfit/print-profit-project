57b16cfd1ffd31bc2a434fc5870a74283e66a6dc
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-02-07 16:19:45 -0600

    Made quote schemas more accurate

--- server/schemas/quotes.js
+++ server/schemas/quotes.js
@@ -5,23 +5,25 @@ const WithId = z.object({
   id: z.coerce.number().positive().int(),
 });
 
+// A lot more of this can be null than I would have thought.
+
 const BaseCost = z.object({
-  name: z.string(),
-  value: z.coerce.number(),
+  name: z.string().max(100),
+  value: z.coerce.number().finite(),
 });
 
 const BaseProduct = z.object({
-  name: z.string(),
-  quantity: z.coerce.number(),
-  selling_price_per_unit: z.coerce.number(),
-  total_selling_price: z.coerce.number().optional(),
-  estimated_hours: z.coerce.number(),
+  name: z.string().max(100),
+  quantity: z.coerce.number().int().optional(),
+  selling_price_per_unit: z.coerce.number().finite().optional(),
+  total_selling_price: z.coerce.number().finite().optional(),
+  estimated_hours: z.coerce.number().int().optional(),
 });
 
 const BaseQuote = z.object({
-  name: z.string(),
-  manual_contribution_percent: z.coerce.number().optional(),
-  manual_total_selling_price: z.coerce.number().optional(),
+  name: z.string().max(100),
+  manual_contribution_percent: z.coerce.number().nonnegative().int().optional(),
+  manual_total_selling_price: z.coerce.number().finite().optional(),
 });
 
 // This is done via merging so update routes can also construct a schema.
