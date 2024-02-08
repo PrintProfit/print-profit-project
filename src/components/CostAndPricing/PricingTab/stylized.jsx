19eb409fb5938421763a40a37707d25cbe3628ba
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-02-07 19:20:33 -0600

    Added border to the right side of the header cells

--- src/components/CostAndPricing/PricingTab/stylized.jsx
+++ src/components/CostAndPricing/PricingTab/stylized.jsx
@@ -1,7 +1,11 @@
-import { TableRow, styled } from '@mui/material';
+// @ts-check
+import { TableRow, styled, tableCellClasses } from '@mui/material';
 
 export const PricingTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(even)': {
     backgroundColor: theme.palette.action.hover,
+  },
+  [`& > .${tableCellClasses.head}`]: {
+    borderRight: `1px solid ${theme.palette.divider}`,
   },
 }));
