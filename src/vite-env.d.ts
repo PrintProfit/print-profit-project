e4b04a97586b0c980f457f371be3a87e9fc1d89c
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-02-08 11:04:15 -0600

    Added environment variables relataed to EmailJS

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,11 @@
 /// <reference types="vite/client" />
 // This file helps VSCode with intellisense.
 // namely, it makes the import.meta.env object work.
+
+// These are additional environment variables needed in .env files.
+interface ImportMetaEnv {
+  /** The service ID for EmailJS */
+  readonly EMAILJS_SERVICE_ID: string;
+  /** The public key for EmailJS */
+  readonly EMAILJS_PUBLIC_KEY: string;
+}
