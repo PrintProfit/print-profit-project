1201a4b27ac32055ae095debdbdfd39827cb62fc
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-02-09 15:23:05 -0600

    Redid quote schema so that repeated data structures are derived from the same schema

--- server/schemas/quotes.js
+++ server/schemas/quotes.js
@@ -6,45 +6,30 @@ const WithId = z.object({
   id: Serial,
 });
 
+// Repeated data types
+const UFloat = z.coerce.number().nonnegative().finite().safe();
+const UInt = z.coerce.number().int().nonnegative();
+// Names could be included, but they aren't inherently the exact same type
+
 // A lot more of this can be null than I would have thought.
 
 const BaseCost = z.object({
   name: z.string().trim().min(1).max(100),
-  value: z.coerce.number().nonnegative().finite().safe(),
+  value: UFloat,
 });
 
 const BaseProduct = z.object({
   name: z.string().trim().min(1).max(100),
-  quantity: z.coerce.number().int().nonnegative().optional(),
-  selling_price_per_unit: z.coerce
-    .number()
-    .nonnegative()
-    .finite()
-    .safe()
-    .optional(),
-  total_selling_price: z.coerce
-    .number()
-    .nonnegative()
-    .finite()
-    .safe()
-    .optional(),
-  estimated_hours: z.coerce.number().int().nonnegative().optional(),
+  quantity: UInt.optional(),
+  selling_price_per_unit: UFloat.optional(),
+  total_selling_price: UFloat.optional(),
+  estimated_hours: UInt.optional(),
 });
 
 const BaseQuote = z.object({
   name: z.string().trim().min(1).max(100),
-  manual_contribution_percent: z.coerce
-    .number()
-    .int()
-    .nonnegative()
-    .max(100)
-    .optional(),
-  manual_total_selling_price: z.coerce
-    .number()
-    .nonnegative()
-    .finite()
-    .safe()
-    .optional(),
+  manual_contribution_percent: UInt.max(100).optional(),
+  manual_total_selling_price: UFloat.optional(),
 });
 
 // This is done via merging so update routes can also construct a schema.
