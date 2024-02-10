453633f219b0f2a3042ba0aba2d1536484122535
Jacob Larson <hockeybobber@comcast.net>
2024-02-10 15:06:23 -0600

    changed 'username' to email in the error reducer

--- src/redux/reducers/errors.reducer.js
+++ src/redux/reducers/errors.reducer.js
@@ -7,13 +7,13 @@ const loginMessage = (state = '', action) => {
     case 'CLEAR_LOGIN_ERROR':
       return '';
     case 'LOGIN_INPUT_ERROR':
-      return 'Enter your username and password!';
+      return 'Enter your email and password!';
     case 'LOGIN_FAILED':
-      return "Oops! The username and password didn't match. Try again!";
+      return "Oops! The email and password didn't match. Try again!";
     case 'LOGIN_FAILED_NO_CODE':
       return 'Oops! Something went wrong! Is the server running?';
     case 'LOGIN_FAILED_PASSWORDS_DONT_MATCH':
-      return `Oops! The username and password didn't match. Try again!`;
+      return `Oops! The email and password didn't match. Try again!`;
     default:
       return state;
   }
@@ -26,11 +26,11 @@ const registrationMessage = (state = '', action) => {
     case 'CLEAR_REGISTRATION_ERROR':
       return '';
     case 'REGISTRATION_INPUT_ERROR':
-      return 'Choose a username and password!';
+      return 'Choose a email and password!';
     case 'REGISTRATION_FAILED':
-      return "Oops! That didn't work. The username might already be taken. Try again!";
+      return "Oops! That didn't work. The email might already be taken. Try again!";
     case 'REGISTRATION_FAILED_PASSWORDS_DONT_MATCH':
-      return `Oops! The username and password didn't match. Try again!`;
+      return `Oops! The passwords didn't match. Try again!`;
     default:
       return state;
   }
