ee0747151621cb38bad1c817dafb915b73687127
Marcos Andrade <mnandrade1999@gmail.com>
2024-02-11 14:11:40 -0600

    Tooltip for Totals table

--- src/components/PricingToolHelp/TotalsHelp.jsx
+++ src/components/PricingToolHelp/TotalsHelp.jsx
@@ -1,5 +1,6 @@
 import { Help } from '@mui/icons-material';
 import {
+  Divider,
   Fade,
   IconButton,
   Modal,
@@ -32,7 +33,23 @@ export function TotalsHelp() {
             }}
           >
             <Typography variant="h6">Totals Help</Typography>
-            <Typography sx={{ mt: 2 }}>TODO Totals</Typography>
+            <Divider />
+            <Typography sx={{ mt: 2 }}>
+              <p>
+                This table is simply an extension of the main pricing tool
+                table. However, This table provides the benefit of having the
+                first column costs close together with the total values, and the
+                added feature to experiment with different values of
+                contribution %, and Total selling price.
+                <br />
+                <br />
+                This table allows you to see the effect that a specific
+                contribution %, or a manually input total selling price has on
+                the total profit.
+                <br />
+                These rows have been highlighted green to indicate importance.
+              </p>
+            </Typography>
           </Paper>
         </Fade>
       </Modal>
