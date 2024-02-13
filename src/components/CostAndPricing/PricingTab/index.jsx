503013878a4be15952eed917247b5b0b53369786
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-02-12 20:20:16 -0600

    Added a keyboard shortcut to the pricing tab to quickly skip inputting stuff in the pricing tool

--- src/components/CostAndPricing/PricingTab/index.jsx
+++ src/components/CostAndPricing/PricingTab/index.jsx
@@ -1,8 +1,10 @@
 // @ts-check
-import { useState } from 'react';
+import { produce } from 'immer';
+import { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { PricingTable } from './PricingTable';
 import { repairQuote } from './data-repair';
+import { toCostNames, unique } from './utils';
 
 export function PricingTab() {
   /** @type {import('./data-types').Quote} */
@@ -10,6 +12,42 @@ export function PricingTab() {
     repairQuote(state.quote.current),
   );
   const [quote, setQuote] = useState(currentQuote);
+
+  const demoInput = useCallback(() => {
+    setQuote(
+      produce((draft) => {
+        const costNames = draft.products.flatMap(toCostNames).filter(unique);
+        if (draft.products[3]) {
+          const product = draft.products[3];
+          product.quantity = 100;
+          product.estimated_hours = 10;
+          product.selling_price_per_unit = 40;
+          product.total_selling_price = 3950;
+          product.costs = costNames.map((name) => ({
+            name,
+            value: Math.round(Math.random() * 1000),
+          }));
+        }
+      }),
+    );
+  }, []);
+
+  useEffect(() => {
+    /**
+     * @param {KeyboardEvent} e
+     */
+    const onKeyDown = (e) => {
+      if (e.key === 'k' && (e.ctrlKey || e.metaKey)) {
+        e.preventDefault();
+        demoInput();
+      }
+    };
+
+    document.addEventListener('keydown', onKeyDown);
+    return () => {
+      document.removeEventListener('keydown', onKeyDown);
+    };
+  }, [demoInput]);
 
   return <PricingTable quote={quote} setQuote={setQuote} />;
 }
