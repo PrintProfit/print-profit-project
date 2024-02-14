1c710604a1562ad25d54ea2109333fec86868b87
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-02-13 19:04:04 -0600

    Added more comments to the index.jsx for the pricing tab

--- src/components/CostAndPricing/PricingTab/index.jsx
+++ src/components/CostAndPricing/PricingTab/index.jsx
@@ -4,11 +4,29 @@ import { useSelector } from 'react-redux';
 import { PricingTable } from './PricingTable';
 import { repairQuote } from './data-repair';
 
+/**
+ * The whole pricing tab.
+ *
+ * It has the pricing table, but it's also a good place for anything like
+ * shortcuts to be put.
+ *
+ * @returns {JSX.Element}
+ */
 export function PricingTab() {
-  /** @type {import('./data-types').Quote} */
+  /**
+   * The initial quote to load into the pricing table.
+   *
+   * The state ends up detached from the redux store, as once the quote is in
+   * the pricing table, we need near-constant access to it to perform any edits
+   * to it.
+   *
+   * @type {import('./data-types').Quote}
+   */
   const currentQuote = useSelector((/** @type {any} */ state) =>
     repairQuote(state.quote.current),
   );
+
+  /** Internal state for the quote */
   const [quote, setQuote] = useState(currentQuote);
 
   return <PricingTable quote={quote} setQuote={setQuote} />;
