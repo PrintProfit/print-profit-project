8bb06e93b55b2f5711326c0e3c3eb8c14b92157a
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-02-14 17:23:51 -0600

    Moved account info into a separate component from the account page

--- src/components/MyAccountPage/MyAccountPage.jsx
+++ src/components/MyAccountPage/MyAccountPage.jsx
@@ -1,23 +1,13 @@
 import { Edit as EditIcon } from '@mui/icons-material';
-import {
-  Button,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableRow,
-  Typography,
-} from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import MyAccountPageForm from '../MyAccountPageForm/MyAccountPageForm';
+import { AccountInfo } from './AccountInfo';
 import './MyAccountPage.css';
 
 export default function MyAccountPage() {
   const dispatch = useDispatch();
-
-  const profileUser = useSelector((store) => store.user.profileUserReducer);
-  const currentUser = useSelector((store) => store.user.currentUser);
 
   useEffect(() => {
     dispatch({ type: 'SAGA_FETCH_PROFILE_PAGE_USER' });
@@ -25,61 +15,17 @@ export default function MyAccountPage() {
 
   const [isForm, setIsForm] = useState(false);
 
-  const toggleForm = () => {
-    setIsForm(true);
-  };
-
-  const displayText = () => {
-    if (isForm) {
-      return (
-        <>
-          <MyAccountPageForm setIsForm={setIsForm} sx={{ mt: 5 }} />
-        </>
-      );
-    }
-    if (isForm === false) {
-      return (
-        <>
-          <TableContainer>
-            <Table>
-              <TableBody>
-                <TableRow>
-                  <TableCell>Name:</TableCell>
-                  <TableCell>{currentUser.name}</TableCell>
-                </TableRow>
-
-                <TableRow>
-                  <TableCell>Email:</TableCell>
-                  <TableCell>{profileUser.email}</TableCell>
-                </TableRow>
-
-                <TableRow>
-                  <TableCell>Company:</TableCell>
-                  <TableCell>{profileUser.company_name}</TableCell>
-                </TableRow>
-
-                {/* <TableRow>
-                  <TableCell align="right" >
-                    <Button
-                      aria-label="edit info"
-                      className="editInfoButton"
-                      onClick={toggleForm}
-                      type="button"
-                      variant="contained"
-                      
-                    >
-                      <Typography> Edit info</Typography>
-
-                      <EditIcon sx={{ color: 'black' }} />
-                    </Button>
-                  </TableCell>
-                </TableRow> */}
-              </TableBody>
-            </Table>
+  return (
+    <div className="myAccountPageCss">
+      <div className="accountPageFormArea">
+        {isForm ? (
+          <MyAccountPageForm setIsForm={setIsForm} />
+        ) : (
+          <AccountInfo>
             <Button
               aria-label="edit info"
               className="editInfoButton"
-              onClick={toggleForm}
+              onClick={() => setIsForm(true)}
               type="button"
               variant="contained"
               sx={{
@@ -90,15 +36,9 @@ export default function MyAccountPage() {
 
               <EditIcon sx={{ color: 'black' }} />
             </Button>
-          </TableContainer>
-        </>
-      );
-    }
-  };
-
-  return (
-    <div className="myAccountPageCss">
-      <div className="accountPageFormArea">{displayText()}</div>
+          </AccountInfo>
+        )}
+      </div>
     </div>
   );
 }
