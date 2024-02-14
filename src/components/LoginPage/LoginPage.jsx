c8afc15887432682b1b1b5b1a022fe0a79d9b795
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-02-14 12:44:45 -0600

    Cleaned up login page

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,14 +1,7 @@
-import { Box } from '@mui/material';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import Paper from '@mui/material/Paper';
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { Box, Paper } from '@mui/material';
 import LoginForm from '../LoginForm/LoginForm';
 
 function LoginPage() {
-  const history = useHistory();
-
   return (
     <Box
       sx={{
