d033922a4cf2dd718ba0f428737cf174221a356a
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-02-13 19:06:32 -0600

    Added more comments to inputs.jsx

--- src/components/CostAndPricing/PricingTab/inputs.jsx
+++ src/components/CostAndPricing/PricingTab/inputs.jsx
@@ -2,24 +2,28 @@
 import { forwardRef } from 'react';
 import { NumericFormat } from 'react-number-format';
 
-/** @type {import('./ref-types').NumericInputRef} */
-export const NumericInput = forwardRef(({ onChange, ...props }, ref) => {
-  return (
-    <NumericFormat
-      {...props}
-      getInputRef={ref}
-      onValueChange={(values) => {
-        onChange({
-          target: {
-            name: props.name,
-            value: values.value,
-          },
-        });
-      }}
-      thousandSeparator
-      valueIsNumericString
-    />
-  );
-});
+/**
+ * An input component for mui's `TextField` that only accepts numeric input.
+ * @see {@link https://mui.com/material-ui/react-text-field/#integration-with-3rd-party-input-libraries | MUI Integration with 3rd-party input libraries}
+ * @type {import('./ref-types').NumericInputRef}
+ */
+export const NumericInput = forwardRef(({ onChange, ...props }, ref) => (
+  <NumericFormat
+    {...props}
+    getInputRef={ref}
+    onValueChange={(values) => {
+      onChange({
+        target: {
+          name: props.name,
+          value: values.value,
+        },
+      });
+    }}
+    thousandSeparator
+    valueIsNumericString
+  />
+));
 
+// Display names are mostly relevant for forward refs. Otherwise, React can
+// infer the name of the component from the variable name.
 NumericInput.displayName = 'NumericInput';
