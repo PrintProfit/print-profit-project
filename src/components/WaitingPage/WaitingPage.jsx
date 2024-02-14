b840a08701d2679b79a427aee5c45e4cae1ea171
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-02-14 16:20:17 -0600

    Rewrote waiting page to use a card

--- src/components/WaitingPage/WaitingPage.jsx
+++ src/components/WaitingPage/WaitingPage.jsx
@@ -1,80 +1,63 @@
-import { ArrowBackIos as ArrowBackIosIcon } from '@mui/icons-material';
-import { Box, Button, Paper, Typography } from '@mui/material';
-import { useHistory } from 'react-router-dom';
+import { ArrowBackIos } from '@mui/icons-material';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Typography,
+} from '@mui/material';
+import { Link } from 'react-router-dom';
 import backgroundImage from '../../assets/printProfitBgLoginFULLv2.png';
 
 export default function WaitingPage() {
-  const history = useHistory();
-
   return (
-    <>
-      <Box
-        sx={{
-          backgroundImage: `url(${backgroundImage})`,
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          margin: 0,
-        }}
-      >
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            pb: 45,
-          }}
-        >
-          <Box
-            sx={{
-              height: 'auto',
-              boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.1)',
-              transition: 'all 0.3s ease-in-out',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: 'primary.light',
-              m: 4,
-              padding: 0,
+    <Box
+      sx={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 0,
+      }}
+    >
+      <Box>
+        <Card raised>
+          <CardHeader
+            title="Thank you for Registering!"
+            titleTypographyProps={{
+              variant: 'h4',
+              fontWeight: 600,
+              textAlign: 'center',
             }}
-          >
-            <Typography
-              sx={{
-                backgroundColor: '#12005C',
-                color: 'white',
-              }}
-              variant="h4"
-            >
-              <h1>Thank you for Registering!</h1>
+          />
+          <CardContent sx={{ textAlign: 'center' }}>
+            <Typography>
+              Your account should be approved in 24-48 hours.
             </Typography>
-            <Paper
-              sx={{
-                width: '100%',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                alignText: 'center',
-              }}
+            <Typography>
+              If you have any questions or pressing concerns, please contact
+              here.
+            </Typography>
+            <Typography>Thank you for your Patience.</Typography>
+          </CardContent>
+          <CardActions sx={{ justifyContent: 'center' }}>
+            <Button
+              variant="contained"
+              color="secondary"
+              component={Link}
+              to="/home"
+              startIcon={<ArrowBackIos />}
             >
-              <Typography variant="h5">
-                <p>Your Account should be approved in 24-48 hours.</p>
-                <p>
-                  If you have any questions or pressing concerns, please contact
-                  here.
-                </p>
-                <p>Thank you for your Patience.</p>
-              </Typography>
-            </Paper>
-          </Box>
-          <Button onClick={() => history.push('/home')}>
-            <ArrowBackIosIcon />
-            <p>Home</p>
-          </Button>
-        </Box>
+              Home
+            </Button>
+          </CardActions>
+        </Card>
       </Box>
-    </>
+    </Box>
   );
 }
